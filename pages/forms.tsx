97934a244c0786e11da5c660aba39d05b086fdc3
xiwoo <xiwoo012@gmail.com>
2022-03-29 19:19:08 +0900

    7.4반영

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,23 +1,42 @@
 
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
 
 export default function Forms() {
-  const {register, watch} = useForm();
-  console.log(watch());
+  const { register, watch, handleSubmit } = useForm<LoginForm>();
+  // console.log(watch());
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby", data);
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log("im invalid bby", errors);
+  };
+
+
   return (
-    <form >
+    <form 
+      onSubmit={handleSubmit(onValid, onInvalid)}
+    >
       <input 
-        {...register("username")}
+        {...register(
+          "username", { required: "히히히히" }
+        )}
         placeholder="username" 
         type="text"
       />
       <input  
-        {...register("email")}
+        {...register("email", { required: true })}
         placeholder="email" 
         type="email" 
       />
       <input  
-        {...register("password")}
+        {...register("password", { required: true })}
         placeholder="password" 
         type="password" 
       />
