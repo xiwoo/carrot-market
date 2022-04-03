519b7a293ffe8d8edfd492841a9330665ab50e22
xiwoo <xiwoo012@gmail.com>
2022-04-03 17:09:26 +0900

    token schema create

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,82 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../../libs/server/withHandler";
+import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  // let user;
+  // if(email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     }
+  //   });
+  //   if(!user) {
+  //     console.log("Did not foun. Will create.");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         email, 
+  //       }
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
+  // if(phone) {
+
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     }
+  //   });
+
+  //   if(!user) {
+  //     console.log("Did not foun. Will create.");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "Anonymous",
+  //         phone: +phone
+  //       }
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if(phone) {
+  //   await client.user.upsert({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //     create: {
+  //       name: "Anonymous",
+  //       phone: +phone,
+  //     },
+  //     update: {},
+  //   });
+  // } else if(email) {
+  //   await client.user.upsert({
+  //     where: {
+  //       email,
+  //     },
+  //     create: {
+  //       name: "Anonymous",
+  //       email,
+  //     },
+  //     update: {},
+  //   });
+  // }
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
   res.status(200).end();
 }
 
