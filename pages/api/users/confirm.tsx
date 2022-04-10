bac3dbb96fd0479b749575b2bca22a51d1694046
xiwoo <xiwoo012@gmail.com>
2022-04-10 17:31:07 +0900

    finish authentication

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,12 +1,12 @@
-import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  console.log(req.session);
+
   const { token } = req.body;
-  const exists = await client.token.findUnique(
+  const foundToken = await client.token.findUnique(
     {
       where: {
         payload: token,
@@ -15,20 +15,18 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
     }
   );
 
-  if(!exists) res.status(404).end();
+  if(!foundToken) return res.status(404).end();
 
-  console.log(exists);
-
-  req.session.user = {
-    id: exists?.userId
-  };
+  req.session.user = { id: foundToken.userId, };
 
   await req.session.save();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId
+    }
+  })
 
-  res.status(200).end();
+  res.json({ok: true});
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
-  cookieName: "carrotsession",
-  password: "dsfasfasdf93142423423sa;lkdjfljsakfknvmlasidfaslejasdlfisaenfasdklafdsk"
-});
+export default withApiSession(withHandler("POST", handler));
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import { useRouter } from "next/router";
 
 interface EnterForm {
   email?: string;
@@ -36,7 +37,6 @@ const Enter: NextPage = () => {
     setMethod("phone");
   };
   const onValid = (validForm: EnterForm) => {
-    // setSubmitting(true);
     enter(validForm);
   };
 
@@ -44,8 +44,12 @@ const Enter: NextPage = () => {
     if(tokenLoading) return;
     confirmToken(validForm);
   }
-
-  console.log(data);
+  const router = useRouter();
+  useEffect(() => {
+      if(tokenData?.ok) {
+        router.push("/");
+      }
+    }, [tokenData, router]);
 
   return (
     <div className="mt-16 px-4">
