a45ff030bd89fef8ddf59bfd2d0c189afd58081a
xiwoo <xiwoo012@gmail.com>
2022-04-26 22:58:50 +0900

    schema add model(review, sale, purchase) and user/me api

---
+++ pages/api/reviews.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse<ResponseType>
+) {
+  
+  const { 
+    query: { id, },
+    session: { user, }
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdFroId: user?.id,
+    },
+    include: {
+      createdBy: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        }
+      }
+    }
+  });
+
+  res.json({
+    ok: true,
+    reviews,
+  });
+
+}
+
+export default withApiSession(withHandler({
+  methods: ["GET"], 
+  handler,
+}));
--- pages/api/users/me/favs.ts
+++ pages/api/users/me/favs.ts
@@ -3,26 +3,29 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
 import { withApiSession } from "@libs/server/withSession";
 
-declare module "iron-session" {
-  interface IronSessionData {
-    user? : {
-      id: number;
-    }
-  }
-}
-
-async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-
-  const profile = await client.user.findUnique({
+async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: {
+      user
+    },
+  } = req;
+  
+  const favs = await client.fav.findMany({
     where: {
-      id: req.session.user?.id
-    }
-  });
+      userId: user?.id
+    },
+    include: {
+      product: true
+    },
+  })
 
   
   res.json({
     ok: true,
-    profile
+    favs
   });
 }
 
